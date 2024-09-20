40c44452b3c8401ca1e411b9bb77121460490685
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2024-09-20 08:54:55 +0000

    base matmul

--- main.cpp
+++ main.cpp
@@ -12,18 +12,67 @@
 #include <numeric>
 #include <cstdint>
 #include <cstring>
+#include <random>
 #include "KernelArguments.hpp"
 #include "cblas.h"
 
-template<typename Ti, typename To>
-hipError_t launchASMCopy(hipFunction_t func, To *out, Ti* in, std::uint32_t length) {
+template <typename T>
+void dumpBuffer(const char* title, const std::vector<T>& data, int M, int N)
+{
+    std::cout << "----- " << title << " start -----" << std::endl;
+    for (int n=0; n<N; n++)
+    {
+        for (int m=0 ; m<M; m++)
+        {
+            std::cout << float(data[m+n*M]) << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "----- " << title << " end -------" << std::endl << std::endl;
+}
 
+template<typename T>
+void initialization(std::vector<T>& data, int M, int N)
+{
+    static std::mt19937 seed(69069);
+
+    for (int n=0; n<N; n++)
+    {
+        for (int m=0 ; m<M; m++)
+        {
+//            data[m+n*M] = T(1.0f);
+            data[m+n*M] = T(float(std::uniform_int_distribution<unsigned>(1, 4)(seed)));
+        }
+    }
+}
+
+template <typename T>
+void CPUMatMul(std::vector<float>& C, const std::vector<T>& A, const std::vector<T>& B, int M, int N, int K)
+{
+    for(int n=0; n<N; n++)
+    {
+        for(int m=0; m<M; m++)
+        {
+            for(int k=0; k<K; k++)
+            {
+                C[n*M+m] = C[n*M+m] + (float(A[m*K+k]) * float(B[n*K+k]));
+            }
+        }
+    }
+}
+
+template<typename T>
+hipError_t launchASMMatMul(hipFunction_t func, float* gpuC, T* gpuA, T* gpuB, int M, int N, int K)
+{
     std::uint32_t workgroups = 1;
 
     KernelArguments args;
-    args.append(out);
-    args.append(in);
-    args.append(length);
+    args.append(gpuC);
+    args.append(gpuA);
+    args.append(gpuB);
+    args.append(M);
+    args.append(N);
+    args.append(K);
     args.applyAlignment();
 
     std::size_t argsSize = args.size();
@@ -59,66 +108,92 @@ hipError_t prepareASMKernel(const std::string &funcName, const std::string &coPa
 }
 
 template <typename T>
-void Sample(const std::string& coPath, const std::uint32_t& length)
+void GPUMatMul(std::vector<float>& C, std::vector<T>& A, std::vector<T>& B, int M, int N, int K)
 {
     hipDevice_t dev{};
     auto err = hipDeviceGet(&dev, 0);
 
-    std::vector<T> cpuRef(length);
-    std::vector<T> cpuOutput(length);
-    std::vector<T> cpuInput(length);
-    for(int i=0; i<length; i++)
-        cpuInput[i] = T(i+1);
+    float* gpuC = nullptr;
+    T*     gpuA = nullptr;
+    T*     gpuB = nullptr;
+    err = hipMalloc(&gpuC, sizeof(float) * M * N);
+    err = hipMalloc(&gpuA, sizeof(T) * K * M);
+    err = hipMalloc(&gpuB, sizeof(T) * K * N);
 
-    std::memcpy(cpuRef.data(), cpuInput.data(), cpuInput.size() * sizeof(T));
+    err = hipMemset(gpuC, 0, sizeof(float) * M * N);
 
-    for (std::size_t i = 0; i < length; ++i) {
-        std::cout << "Tony Ref " << float(cpuInput[i]) << std::endl;
-    }
-
-    T *gpuOutput{};
-    err = hipMalloc(&gpuOutput, sizeof(T) * length);
-    err = hipMemset(gpuOutput, 0, sizeof(T) * length);
-
-    T *gpuInput{};
-    err = hipMalloc(&gpuInput, sizeof(T) * length);
-    err = hipMemcpyHtoD(gpuInput, cpuInput.data(), sizeof(T) * length);
+    err = hipMemcpyHtoD(gpuA, A.data(), sizeof(T) * K * M);
+    err = hipMemcpyHtoD(gpuB, B.data(), sizeof(T) * K * N);
 
     hipModule_t module{};
     hipFunction_t func{};
 
-    err = prepareASMKernel("Copy", coPath, &module, &func);
+    err = prepareASMKernel("MatMul", "matmul.co", &module, &func);
     if (err)
         std::cout << "find asm kernel failed" << std::endl;
 
-    err = launchASMCopy(func, gpuOutput, gpuInput, length);
+    err = launchASMMatMul(func, gpuC, gpuA, gpuB, M, N, K);
     if (err)
-        std::cout << "launchASMCopy error : " << err << std::endl;
+        std::cout << "launchASMMatMul error : " << err << std::endl;
 
-    err = hipMemcpyDtoH(cpuOutput.data(), gpuOutput, cpuInput.size() * sizeof(T));
+    err = hipMemcpyDtoH(C.data(), gpuC, sizeof(float) * M * N);
 
-    for (std::size_t i = 0; i < length; ++i) {
-        std::cout << "Tony GPU " << float(cpuOutput[i]) << std::endl;
-    }
-
-    err = hipFree(gpuOutput);
-    err = hipFree(gpuInput);
     err = hipModuleUnload(module);
+    err = hipFree(gpuC);
+    err = hipFree(gpuA);
+    err = hipFree(gpuB);
+}
+
+template <typename T>
+void validate(const std::vector<T>& cpuR, const std::vector<T>& cpuC, int M, int N)
+{
+    for (int n=0; n<N; n++)
+    {
+        for (int m=0 ; m<M; m++)
+        {
+            float err = std::abs(float(cpuR[m+n*M]) - float(cpuC[m+n*M]));
+            if (err > 1e-5)
+            {
+                std::cout << "Error " << err << " Ref " << float(cpuR[m+n*M]) << " GPU " << cpuC[m+n*M] << std::endl;
+                return;
+            }
+        }
+    }
+    std::cout << "PASS" << std::endl;
+}
+
+template <typename T>
+void Sample(const std::uint32_t M, const std::uint32_t N, const std::uint32_t K)
+{
+    std::vector<float> cpuR(M * N, 0.0f);
+    std::vector<float> cpuC(M * N, 0.0f);
+    std::vector<T> cpuA(K * M);
+    std::vector<T> cpuB(K * N);
+
+    initialization(cpuA, K, M);
+    initialization(cpuB, K, N);
+ 
+    CPUMatMul(cpuR, cpuA, cpuB, M, N, K);
+    GPUMatMul(cpuC, cpuA, cpuB, M, N, K);
+ 
+    dumpBuffer("A", cpuA, K, M);
+    dumpBuffer("B", cpuB, K, N);
+    dumpBuffer("C", cpuC, M, N);
+    dumpBuffer("R", cpuR, M, N);
+ 
+    validate(cpuR, cpuC, M, N);
 }
 
 int main(int argc, char **argv) {
 
-    if (argc != 2)
+    if (argc != 1)
     {
-        std::cout << "./copy [length]" << std::endl;
+        std::cout << "./matmul" << std::endl;
         return -1;
     }
 
-    const std::uint32_t length(std::atoi(argv[1]));
-
-    Sample<__hip_fp8_e4m3_fnuz>("copy.co", length);
-
-    std::cout << "Tony sizeof __hip_fp8_e4m3_fnuz " << sizeof(__hip_fp8_e4m3_fnuz) << std::endl;
+    Sample<__hip_fp8_e4m3_fnuz>(16, 16, 32);
 
     return 0;
+
 }
